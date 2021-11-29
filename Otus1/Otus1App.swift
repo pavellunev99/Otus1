1fed780149ae016a2e056dbd2f79657579979786
Pavel Lunev <69137090+pavellunevIncrdbl@users.noreply.github.com>
2021-11-29 22:23:55 +0500

    Initial Commit

---
+++ Otus1/Otus1App.swift
@@ -0,0 +1,17 @@
+//
+//  Otus1App.swift
+//  Otus1
+//
+//  Created by Павел Лунев on 29.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct Otus1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
